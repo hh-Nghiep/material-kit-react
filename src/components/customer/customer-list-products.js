376ee70fc3cac978d0e32cba419af37a0c41d1f2
Từ Thái Phong <65271530+thaiphong-ptit@users.noreply.github.com>
2022-05-14 19:08:36 +0700

    update crud product

--- src/components/customer/customer-list-products.js
+++ src/components/customer/customer-list-products.js
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import NextLink from "next/link";
 import PerfectScrollbar from "react-perfect-scrollbar";
+import * as Yup from "yup";
 import PropTypes from "prop-types";
 import { Menu, Dropdown } from "antd";
 import { EditOutlined, DeleteOutlined, MoreOutlined } from "@ant-design/icons";
@@ -7,41 +9,98 @@ import "antd/dist/antd.css";
 import {
   Avatar,
   Box,
+  Button,
   Card,
+  Modal,
   Table,
   TableBody,
   TableCell,
   TableHead,
   TablePagination,
   TableRow,
+  TextField,
   Typography,
+  Link,
 } from "@mui/material";
 import { getInitials } from "../../utils/get-initials";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+// import Modal from "react-modal";
 
-export const CustomerListProducts = ({ customers, ...rest }) => {
+export const CustomerListProducts = ({ products, ...rest }) => {
   const [selectedCustomerIds, setSelectedCustomerIds] = useState([]);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleOpen = (e) => {
+    console.log("click", e.key);
+    console.log("producs", products);
+    let selected = products?.filter((x) => x.id == e.key);
+    formik.setFieldValue("code", selected[0].id);
+    formik.setFieldValue("name", selected[0].name);
+    formik.setFieldValue("price", selected[0].price);
+    formik.setFieldValue("detailDescription", selected[0].detailDescription);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
+  const [selectedProduct, setSelectedProduct] = useState({});
 
+  // get selected product
+
+  const router = useRouter();
+  const formik = useFormik({
+    initialValues: {
+      username: "user3",
+      password: "123",
+    },
+    // validationSchema: Yup.object({
+    //   username: Yup.string()
+    //     // .email(
+    //     //   'Must be a valid email')
+    //     .max(255)
+    //     .required("Username is required"),
+    //   password: Yup.string().max(255).required("Password is required"),
+    // }),
+    onSubmit: (e) => {
+      const payload = {
+        code: e?.code,
+        name: e?.name,
+        shortDescription: e?.detailDescription,
+        detailDescription: e?.detailDescription,
+        price: e?.price,
+        gender: 1,
+        activeFlag: true,
+        competitivePrice: e?.price,
+        sizes: ["S", "L", "XL", "XXL"],
+        colors: ["white", "black", "blue"],
+      };
+
+      console.log(e);
+      // login(e);
+    },
+  });
   function handleMenuClick(e) {
-    message.info("Click on menu item.");
-    console.log("click", e);
+    // message.info("Click on menu item.");
+
+    setModalIsOpen(true);
   }
 
   const menu = (
     <Menu
-      onClick={handleMenuClick}
       items={[
         {
           label: "Edit",
           key: "1",
           icon: <EditOutlined />,
+          onClick: handleOpen,
         },
         {
           label: "Delete",
           key: "2",
           icon: <DeleteOutlined />,
           style: { color: "red" },
+          onClick: { handleMenuClick },
         },
       ]}
     />
@@ -54,80 +113,202 @@ export const CustomerListProducts = ({ customers, ...rest }) => {
   const handlePageChange = (event, newPage) => {
     setPage(newPage);
   };
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
   return (
-    <Card {...rest}>
-      <PerfectScrollbar>
-        <Box sx={{ minWidth: 1050 }}>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Image</TableCell>
-                <TableCell>Name</TableCell>
-                <TableCell>Price</TableCell>
-                <TableCell>Amount</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {customers.slice(0, limit).map((customer) => (
-                <TableRow
-                  hover
-                  key={customer.id}
-                  selected={selectedCustomerIds.indexOf(customer.id) !== -1}
-                >
-                  <TableCell>
-                    <Avatar src={customer.avatarUrl} sx={{ mr: 2 }}>
-                      {getInitials(customer.name)}
-                    </Avatar>
-                  </TableCell>
-                  <TableCell>
-                    <Box
-                      sx={{
-                        alignItems: "center",
-                        display: "flex",
-                      }}
-                    >
-                      <Typography color="textPrimary" variant="body1">
-                        {customer.name}
-                      </Typography>
-                    </Box>
-                  </TableCell>
-                  <TableCell>{customer.email}</TableCell>
-                  <TableCell>
-                    {`${customer.address.city}, ${customer.address.state}, ${customer.address.country}`}
-                  </TableCell>
-                  <TableCell>
-                    <Dropdown overlay={menu} trigger={["click"]}>
-                      <a onClick={(e) => e.preventDefault()}>
-                        <MoreOutlined
-                          style={{
-                            border: "1px solid #d9d9d9",
-                            color: "black",
-                            padding: "7px",
-                            marginLeft: "10px",
-                            fontSize: "18px"
-                          }}
-                        />
-                      </a>
-                    </Dropdown>
-                  </TableCell>
+    <>
+      <Card {...rest}>
+        <PerfectScrollbar>
+          <Box sx={{ minWidth: 1050 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Image</TableCell>
+                  <TableCell>Name</TableCell>
+                  <TableCell>Price</TableCell>
+                  <TableCell>Description</TableCell>
+                  <TableCell>Action</TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+              </TableHead>
+              <TableBody>
+                {products?.map((product) => (
+                  <TableRow
+                    hover
+                    key={product?.id}
+                    selected={selectedCustomerIds.indexOf(product?.id) !== -1}
+                  >
+                    <TableCell>
+                      <Avatar src={product?.avatarUrl} sx={{ mr: 2 }}>
+                        {getInitials(product?.name)}
+                      </Avatar>
+                    </TableCell>
+                    <TableCell>
+                      <Box
+                        sx={{
+                          alignItems: "center",
+                          display: "flex",
+                        }}
+                      >
+                        <Typography color="textPrimary" variant="body1">
+                          {product?.name}
+                        </Typography>
+                      </Box>
+                    </TableCell>
+                    <TableCell>{product?.price}</TableCell>
+                    <TableCell>{product?.detailDescription}</TableCell>
+                    <TableCell>
+                      <Dropdown overlay={menu} trigger={["click"]}>
+                        <a
+                          onClick={(e) => {
+                            e.preventDefault();
+                            console.log("value", e);
+                          }}
+                        >
+                          <MoreOutlined
+                            style={{
+                              border: "1px solid #d9d9d9",
+                              color: "black",
+                              padding: "7px",
+                              marginLeft: "10px",
+                              fontSize: "18px",
+                            }}
+                          />
+                        </a>
+                      </Dropdown>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </PerfectScrollbar>
+        <TablePagination
+          component="div"
+          count={products?.length}
+          onPageChange={handlePageChange}
+          onRowsPerPageChange={handleLimitChange}
+          page={page}
+          rowsPerPage={limit}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
+      </Card>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <form onSubmit={formik.handleSubmit}>
+            <Box sx={{ my: 3 }}>
+              <Typography color="textPrimary" variant="h4">
+                Update product
+              </Typography>
+            </Box>
+
+            <TextField
+              error={Boolean(formik.touched.code && formik.errors.code)}
+              fullWidth
+              helperText={formik.touched.code && formik.errors.code}
+              label="Code"
+              margin="normal"
+              name="code"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="text"
+              value={formik.values.code}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.name && formik.errors.name)}
+              fullWidth
+              helperText={formik.touched.name && formik.errors.name}
+              label="Name"
+              margin="normal"
+              name="name"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="text"
+              value={formik.values.name}
+              variant="outlined"
+            />
+
+            <TextField
+              error={Boolean(formik.touched.price && formik.errors.price)}
+              fullWidth
+              helperText={formik.touched.price && formik.errors.price}
+              label="Price"
+              margin="normal"
+              name="price"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="text"
+              value={formik.values.price}
+              variant="outlined"
+            />
+            <TextField
+              error={Boolean(formik.touched.detailDescription && formik.errors.detailDescription)}
+              fullWidth
+              helperText={formik.touched.detailDescription && formik.errors.detailDescription}
+              label="Detail description"
+              margin="normal"
+              name="detailDescription"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="text"
+              value={formik.values.detailDescription}
+              variant="outlined"
+            />
+            {/* <TextField
+              error={Boolean(formik.touched.gender && formik.errors.gender)}
+              fullWidth
+              helperText={formik.touched.gender && formik.errors.gender}
+              label="Gender"
+              margin="normal"
+              name="gender"
+              onBlur={formik.handleBlur}
+              onChange={formik.handleChange}
+              type="text"
+              value={formik.values.gender}
+              variant="outlined"
+            /> */}
+            <Box sx={{ py: 2, ml: 16 }}>
+              <Button
+                sx={{ mr: 2 }}
+                color="primary"
+                // disabled={formik.isSubmitting}
+                // fullWidth
+                size="large"
+                type="submit"
+                variant="contained"
+              >
+                Update
+              </Button>
+              <Button
+                color="primary"
+                // fullWidth
+                size="large"
+                // type="submit"
+                variant="contained"
+                onClick={handleClose}
+              >
+                close
+              </Button>
+            </Box>
+          </form>
         </Box>
-      </PerfectScrollbar>
-      <TablePagination
-        component="div"
-        count={customers.length}
-        onPageChange={handlePageChange}
-        onRowsPerPageChange={handleLimitChange}
-        page={page}
-        rowsPerPage={limit}
-        rowsPerPageOptions={[5, 10, 25]}
-      />
-    </Card>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -61,46 +61,9 @@ const Login = () => {
               <Typography color="textPrimary" variant="h4">
                 Sign in
               </Typography>
-              <Typography color="textSecondary" gutterBottom variant="body2">
-                Sign in on the internal platform
-              </Typography>
+
             </Box>
-            <Grid container spacing={3}>
-              <Grid item xs={12} md={6}>
-                <Button
-                  color="info"
-                  fullWidth
-                  startIcon={<FacebookIcon />}
-                  onClick={formik.handleSubmit}
-                  size="large"
-                  variant="contained"
-                >
-                  Login with Facebook
-                </Button>
-              </Grid>
-              <Grid item xs={12} md={6}>
-                <Button
-                  fullWidth
-                  color="error"
-                  startIcon={<GoogleIcon />}
-                  onClick={formik.handleSubmit}
-                  size="large"
-                  variant="contained"
-                >
-                  Login with Google
-                </Button>
-              </Grid>
-            </Grid>
-            <Box
-              sx={{
-                pb: 1,
-                pt: 3,
-              }}
-            >
-              <Typography align="center" color="textSecondary" variant="body1">
-                or login with username
-              </Typography>
-            </Box>
+
             <TextField
               error={Boolean(formik.touched.username && formik.errors.username)}
               fullWidth
